3b17f295ff847bacf4f6fd1abc26b0adca3dc211
Bryant Truong <Bryanttruong15@gmail.com>
2025-10-21 15:15:10 -0700

    Api

---
+++ backend/src/main/java/com/gearfitness/gear_api/GearFitnessApiApplication.java
@@ -0,0 +1,13 @@
+package com.gearfitness.gear_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearFitnessApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GearFitnessApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gearfitness/gear_api/GearFitnessApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gearfitness.gear_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GearFitnessApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
