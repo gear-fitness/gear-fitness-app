522fd08e0de8279a90c2aff22e579a553c216241
Max Lopez <familiacachorin@gmail.com>
2025-10-28 14:31:04 -0700

    fix data.sql injection

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -4,4 +4,4 @@ INSERT INTO app_user (user_id, username, email, password_hash, is_private, creat
 VALUES (gen_random_uuid(), 'bryant', 'bryant@example.com', 'hashed123', false, NOW());
 
 INSERT INTO exercise (exercise_id, name, body_part, description)
-VALUES (gen_random_uuid(), 'Bench Press', 'chest', 'Push exercise for chest.');
+VALUES (gen_random_uuid(), 'Bench Press', 'CHEST', 'Push exercise for chest.');
