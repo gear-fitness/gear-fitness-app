1e41869531f37ffd4301f0ef3648bbe98818c0f1
Alton <altonmatovu123@gmail.com>
2025-11-06 01:50:23 -0800

    added my ips to the files

--- frontend/src/navigation/screens/Login.tsx
+++ frontend/src/navigation/screens/Login.tsx
@@ -29,7 +29,7 @@ export function LoginScreen() {
         const { idToken, user } = respone.data;
         const backendResponse = await fetch(
           //INSERT YOUR BACKEND URL HERE (e.g., localhost or your server's IP) spring boot server
-          "http://192.168.0.19:8080/api/auth/google",
+          "http://10.58.14.218:8080/api/auth/google",
           {
             method: "POST",
             headers: { "Content-Type": "application/json" },
