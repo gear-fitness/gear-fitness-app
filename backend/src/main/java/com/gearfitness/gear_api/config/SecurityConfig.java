5d8c5ac3f5affcf6aef427496cd4822f1bf6c7c3
Max Lopez <familiacachorin@gmail.com>
2025-12-18 18:15:31 -0800

    remove cors import

--- backend/src/main/java/com/gearfitness/gear_api/config/SecurityConfig.java
+++ backend/src/main/java/com/gearfitness/gear_api/config/SecurityConfig.java
@@ -1,8 +1,5 @@
 package com.gearfitness.gear_api.config;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,9 +10,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.gearfitness.gear_api.security.JwtAuthenticationFilter;
 
@@ -27,9 +21,6 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
-
-    @Value("${cors.allowed-origins}")
-    private String allowedOrigins;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
