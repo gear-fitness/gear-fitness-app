9955f18fc316561244cee0c23ba63c010908083c
Max Lopez <familiacachorin@gmail.com>
2025-12-18 18:13:14 -0800

    remove unecessary cors line

--- backend/src/main/java/com/gearfitness/gear_api/config/SecurityConfig.java
+++ backend/src/main/java/com/gearfitness/gear_api/config/SecurityConfig.java
@@ -35,7 +35,6 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
