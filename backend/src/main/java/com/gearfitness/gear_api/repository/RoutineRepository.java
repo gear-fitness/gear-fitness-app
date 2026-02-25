327c1f43c8a6f0526397f7dca36c542d0887e705
Kobe Cortez <kobecortez31@gmail.com>
2026-02-25 15:42:29 -0800

    finished routine repos file

--- backend/src/main/java/com/gearfitness/gear_api/repository/RoutineRepository.java
+++ backend/src/main/java/com/gearfitness/gear_api/repository/RoutineRepository.java
@@ -4,10 +4,17 @@ import java.util.UUID;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.stereotype.Repository;
-
 import com.gearfitness.gear_api.entity.Routine;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 @Repository
-public interface RoutineRepository {
-    List<Routine> findByUser_IdOrderByCreatedAtDesc(UUID userId);
-    Optional<Routine> findByRoutineId(UUID routineId); //with entity graph for exercises
+public interface RoutineRepository extends JpaRepository<Routine, UUID> {
+    List<Routine> findByUser_UserIdOrderByCreatedAtDesc(UUID userId);
+
+    @EntityGraph(attributePaths = { "routineExercises", "routineExercises.exercise" })
+    @Query("SELECT r FROM Routine r WHERE r.routineId = :routineId")
+    Optional<Routine> findByRoutineId(@Param("routineId") UUID routineId);
 }
