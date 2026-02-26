a4dcba5dfb8d1c32b77739414ea5733a19c1e3b6
Kobe Cortez <kobecortez31@gmail.com>
2026-02-25 16:23:13 -0800

    progress on RoutineService.java

--- backend/src/main/java/com/gearfitness/gear_api/service/RoutineService.java
+++ backend/src/main/java/com/gearfitness/gear_api/service/RoutineService.java
@@ -1,30 +1,34 @@
 package com.gearfitness.gear_api.service;
 
+import java.util.UUID;
+import com.gearfitness.gear_api.dto.CreateRoutineFromWorkoutDTO;
+import com.gearfitness.gear_api.entity.AppUser;
+import com.gearfitness.gear_api.entity.Workout;
 import com.gearfitness.gear_api.repository.RoutineRepository;
-
 import jakarta.transaction.Transactional;
-
 import org.springframework.stereotype.Service;
-
 import com.gearfitness.gear_api.dto.RoutineDTO;
 import com.gearfitness.gear_api.repository.AppUserRepository;
-
 import lombok.RequiredArgsConstructor;
+import com.gearfitness.gear_api.repository.WorkoutRepository;
 
 @Service
 @RequiredArgsConstructor
 public class RoutineService {
     private final RoutineRepository routineRepository;
     private final AppUserRepository appUserRepository;
+    private final WorkoutRepository workoutRepository;
 
     @Transactional
-    public RoutineDTO createFromWorkout(CreateRoutineFromWorkoutDTO dto userId) {
+    public RoutineDTO createFromWorkout(CreateRoutineFromWorkoutDTO dto, UUID userId) {
         AppUser user = appUserRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("User not found"));
-        
-        Workout workout = workoutRepository.findById(dto.getWorkoutId())
+
+        Workout workout = workoutRepository.findByIdWithDetails(dto.getWorkoutId())
                 .orElseThrow(() -> new RuntimeException("Workout not found"));
 
-        if (!workout.getUser())
+        if (!workout.getUser().getUserId().equals(user.getUserId())) {
+            throw new RuntimeException("Workout does not belong to user");
+        }
     }
 }
